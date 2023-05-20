409bf90cc143e51189cb6b47fd7799dcd3a3a03c
Andrew Hatton <andrew@thehattons.co>
2023-05-19 20:17:47 -0400

    Updated code

--- APITest/Calculators/GridReferenceCalculators/IGridReferenceByTriangleCalculator.cs
+++
@@ -1,9 +0,0 @@
-﻿using Triangular.BLL;
-
-namespace APITest.Calculators.GridReferenceCalculators
-{
-    public interface IGridReferenceByTriangleCalculator
-    {
-        GridReference Calculate(IGrid grid, Triangle triangle);
-    }
-}
