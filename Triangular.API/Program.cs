6a88bbd163d25e82033ad8ee3a202e55256952a3
Andrew Hatton <andrew@thehattons.co>
2023-05-20 01:10:09 -0400

    code cleanup

--- Triangular.API/Program.cs
+++ Triangular.API/Program.cs
@@ -21,8 +21,15 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
     app.UseSwaggerUI();
+}
+else if (app.Environment.IsProduction())
+{
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1.0/swagger.json", "v1.0");
+        options.RoutePrefix = "api/swagger";
+    });
 }
 
 app.UseHttpsRedirection();
@@ -32,3 +39,6 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+// This code was developed by and written by myself, Andrew Hatton, for the purpose of the Software
+// Engineer interview task. For questions about this feel free to email me at andrewhatton(at)cronotech.us
